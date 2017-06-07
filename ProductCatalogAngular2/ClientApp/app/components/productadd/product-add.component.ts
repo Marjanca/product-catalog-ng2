fd8a585788b5172f04e4d8c8976021645b42ad09
Marija Popovic <marija.popovic@zuehlke.com>
2017-06-07 13:37:26 +0200

    add component

---
+++ ProductCatalogAngular2/ClientApp/app/components/productadd/product-add.component.ts
@@ -0,0 +1,43 @@
+﻿import { Component, OnInit, Input, Output } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { ActivatedRoute, Params } from '@angular/router';
+import {Location} from '@angular/common';
+
+
+@Component({
+    selector: 'product-add',
+    template: require('./product-add.component.html')
+})
+export class ProductAddComponent implements OnInit{
+    productId: number;
+    currentProduct: Product;
+
+    constructor(private http: Http, private route: ActivatedRoute, private location: Location) {
+    }
+
+    ngOnInit() {
+        this.currentProduct = new Product();
+    }
+
+    addItem() {
+        let body = JSON.stringify(this.currentProduct);
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers }); 
+        this.http.post('/api/Product', body, options).subscribe(result => {
+                this.location.back();
+        });
+    }
+}
+
+export class Product {
+    productID: number;
+    name: string;
+    color: string;
+    price: number;
+    quantity: number;
+    madeIn: string;
+    tags: string;
+
+    constructor(){
+    }
+}
